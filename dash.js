c6b9ac4beaa6ac0e349903428dc711eb334afbe5
Gerald Muhumuza <135716969+geraldmuhumuza@users.noreply.github.com>
2024-07-15 11:40:43 +0300

    Update dash.js

--- dash.js
+++ dash.js
@@ -1,7 +1,10 @@
 
 function openNav() {
-    document.getElementById("Sidebar").style.width = "250px";
+    	document.getElementById("Sidebar").style.width = "250px";
+	document.getElementById("menu").style.marginLeft= "0";
 }
 function closeNav() {
-    document.getElementById("Sidebar").style.width = "0";
+    	document.getElementById("Sidebar").style.width = "0";
+	document.getElementById("content").style.marginLeft= "250px";
+	
 }
